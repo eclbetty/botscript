4d0e192ca18db785dc031a2b50ad26de91a43ee1
greatkia <49657442+eclbetty@users.noreply.github.com>
2020-04-02 16:06:52 +0800

    2nd pw auto fill

---
+++ kiss2nd.js
@@ -0,0 +1,7 @@
+//url: https://kk1.918kiss.com/com/com_CheckPrivatePwd.aspx?accName=*
+
+document.querySelector('#txt_Password').value = '121212';
+setTimeout(() => {
+	
+document.querySelector('#Button_OK').click();
+}, 100);
